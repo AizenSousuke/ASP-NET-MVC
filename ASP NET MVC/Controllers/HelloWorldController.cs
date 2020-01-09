12704352b51ff65648532e08589b96934aec590e
Accelerator <puteranikdimas@yahoo.com.sg>
2020-01-09 21:54:46 +0800

    Added new functions

--- ASP NET MVC/Controllers/HelloWorldController.cs
+++ ASP NET MVC/Controllers/HelloWorldController.cs
@@ -8,19 +8,22 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
             // This is the default part that will load if there is no method name explicitly specified behind /
-            return "This is my default action...";
+            // return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            // return "This is the Welcome action method...";
-            return HtmlEncoder.Default.Encode($"Hello {name}, num times is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
