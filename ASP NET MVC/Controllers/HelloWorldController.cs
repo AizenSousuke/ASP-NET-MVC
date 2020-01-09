1a9171e9696b31df03d041071ab153e372560465
Accelerator <puteranikdimas@yahoo.com.sg>
2020-01-09 20:01:30 +0800

    Added Hello World Controller

---
+++ ASP NET MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            // This is the default part that will load if there is no method name explicitly specified behind /
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            // return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, num times is: {numTimes}");
+        }
+    }
+}
