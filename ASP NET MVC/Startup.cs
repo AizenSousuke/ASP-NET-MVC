9efb163aad6a18caa52cff5acd3a981288a82fc1
Accelerator <puteranikdimas@yahoo.com.sg>
2020-01-10 16:21:01 +0800

    Added database

--- ASP NET MVC/Startup.cs
+++ ASP NET MVC/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ASP_NET_MVC
 {
     public class Startup
@@ -24,6 +27,11 @@ namespace ASP_NET_MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Call database context service
+            services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
